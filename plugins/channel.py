ff96e3ba390996209d02199299a8ee15b4504b6a
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2025-06-14 04:59:28 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -201,7 +201,7 @@ async def get_pixels(caption):
 
 
 async def movie_name_format(file_name):
-  clean_filename = re.sub(r'http\S+', '', re.sub(r'@\w+|#\w+', '', file_name).replace('_', ' ').replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace('{', '').replace('}', '').replace('.', ' ').replace('@', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '')).strip()
+  clean_filename = re.sub(r'http\S+', '', re.sub(r'@\w+|#\w+', '', caption).replace('_', ' ').replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace('{', '').replace('}', '').replace('.', ' ').replace('@', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '').replace('<b>', '').replace('<\b>', '')).strip()
   return clean_filename
 
 
